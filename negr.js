59aceeb19518f03bd4cfb95d5ba8c8ece7d12d38
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-09 11:26:57 +0500

    Add files via upload

---
+++ negr.js
@@ -0,0 +1,17 @@
+let mevalar = prompt("Manashu mevalarni yozing olma banan apelsin")
+
+if (mevalar=olma) {
+    alert("malades,olmani yaxshi korar ekansan")
+}else{
+    alert("manimcha sanda olmaga alergiya bor ")
+}
+if (mevalar=banan) {
+    alert("malades,bananni yaxshi korar ekansan")
+}else{
+    alert("manimcha sanda olmaga alergiya bor ")
+}
+if (mevalar=apelsin) {
+    alert("malades,apelsini yaxshi korar ekansan")
+}else{
+    alert("manimcha sanda olmaga alergiya bor ")
+}
